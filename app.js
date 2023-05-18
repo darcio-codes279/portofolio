0fd032ae55911866181d4c2dbc025eb27ae972dd
darcio-codes279 <darciomassala@gmail.com>
2023-05-18 06:55:46 +0100

    fixed the thingy

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 const toggleMenu = () => {
 	document.body.classList.toggle("open");
 };
+
+function showFunction1() {
+	document.querySelector(".front-end-message").style.display = "block";
+	document.querySelector(".cloud-eng-message").style.display = "none";
+	document.querySelector(".tech-enth-message").style.display = "none";
+	document.querySelector(".speaker-message").style.display = "none";
+}
+
+function showFunction2() {
+	document.querySelector(".cloud-eng-message").style.display = "block";
+	document.querySelector(".front-end-message").style.display = "none";
+	document.querySelector(".tech-enth-message").style.display = "none";
+	document.querySelector(".speaker-message").style.display = "none";
+}
+
+function showFunction3() {
+	document.querySelector(".tech-enth-message").style.display = "block";
+	document.querySelector(".front-end-message").style.display = "none";
+	document.querySelector(".cloud-eng-message").style.display = "none";
+	document.querySelector(".speaker-message").style.display = "none";
+}
+
+function showFunction4() {
+	document.querySelector(".speaker-message").style.display = "block";
+	document.querySelector(".front-end-message").style.display = "none";
+	document.querySelector(".cloud-eng-message").style.display = "none";
+	document.querySelector(".tech-enth-message").style.display = "none";
+}
