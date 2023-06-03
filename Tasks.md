5ea526f8b01ab0963eb275dcb2025a39bcef4606
darcio-codes279 <darciomassala@gmail.com>
2023-06-03 14:31:30 +0100

    New changes

--- Tasks.md
+++ Tasks.md
@@ -9,10 +9,11 @@
 [] change background
 [] fix navigation bar
 [] make sure Mobile layout is fine
+[] Add animations
+[] Fix positioning
 
 ## Hero section
 
-[] Animations
 [] Add links to social media buttons
 [] Add link to home button
 
@@ -23,11 +24,16 @@
 ## Portfolio
 
 [] fix carousel (Swiper)
+[] When I hover over the img, I should get some information
 
 - Or add buttons
-  [✅] Remove the navigation bar - commented out
+
+[✅] Remove the navigation bar - commented out
 
 ## Form
 
 [] Fix layout of form
 [] Add link to submit button
+[] One line underlining the form
+
+[] Diferent
--- app.js
+++ app.js
@@ -53,29 +53,33 @@ function showFunction3() {
 // techEnthBtn.addEventListener("click", showFunction3);
 
 const slider = document.querySelector(".carousel");
+
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener("mousedown", (e) => {
+xfunction = () => {
 	isDown = true;
 	slider.classList.add("active");
 	startX = e.pageX - slider.offsetLeft;
 	scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", (_) => {
-	isDown = false;
-	slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", (_) => {
-	isDown = false;
-	slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", (e) => {
-	if (!isDown) return;
-	e.preventDefault();
-	const x = e.pageX - slider.offsetLeft;
-	const SCROLL_SPEED = 3;
-	const walk = (x - startX) * SCROLL_SPEED;
-	slider.scrollLeft = scrollLeft - walk;
-});
+};
+
+slider.addEventListener();
+// slider.addEventListener("mouseleave", (_) => {
+// 	isDown = false;
+// 	slider.classList.remove("active");
+// 	console.log("clicked");
+// });
+// slider.addEventListener("mouseup", (_) => {
+// 	isDown = false;
+// 	slider.classList.remove("active");
+// });
+// slider.addEventListener("mousemove", (e) => {
+// 	if (!isDown) return;
+// 	e.preventDefault();
+// 	const x = e.pageX - slider.offsetLeft;
+// 	const SCROLL_SPEED = 3;
+// 	const walk = (x - startX) * SCROLL_SPEED;
+// 	slider.scrollLeft = scrollLeft - walk;
+// });
